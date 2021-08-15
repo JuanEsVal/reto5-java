c345844baec46153a39a18eb1eb8ce3399c5e212
JuanEsVal <juanes.valenciag@gmail.com>
2021-08-15 17:43:31 -0500

    Reto 4

---
+++ src/main/java/model/vo/SumaProveedor.java
@@ -0,0 +1,17 @@
+package model.vo;
+
+public class SumaProveedor {
+
+    private int cantidad;
+
+    public SumaProveedor() {
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }    
+}
